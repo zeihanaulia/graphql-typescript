9e40b3f568508c2767eaf67fb9d7cc708dd74d33
Zeihan Aulia <zeihan@warungpintar.co>
2019-03-04 16:16:49 +0700

    move resolvers to resolvers.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import "reflect-metadata";
 import { importSchema } from 'graphql-import';
-
 import { GraphQLServer } from 'graphql-yoga'
+import { resolvers } from './resolvers';
 // ... or using `require()`
 // const { GraphQLServer } = require('graphql-yoga')
 
 const typeDefs = importSchema('schema.graphql');
 
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
-  },
-}
+
 
 const server = new GraphQLServer({ typeDefs, resolvers })
 server.start(() => console.log('Server is running on localhost:4000'))
