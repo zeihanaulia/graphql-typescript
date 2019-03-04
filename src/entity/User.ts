1c89d6a0473f85d87da4455be229bfd1b130b6c1
Zeihan Aulia <zeihan@warungpintar.co>
2019-03-04 15:57:40 +0700

    add npm install uuid

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -3,8 +3,8 @@ import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 @Entity()
 export class User {
 
-    @PrimaryGeneratedColumn()
-    id: number;
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
 
     @Column()
     firstName: string;
