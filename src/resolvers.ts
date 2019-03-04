329c0a8701b05bbb26d9b98cbcb73c5505ed8995
Zeihan Aulia <zeihan@warungpintar.co>
2019-03-04 17:19:50 +0700

    create user and store to database

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,4 +1,6 @@
+import * as bcrypt from "bcryptjs";
 import { ResolverMap } from "./types/graphql-utils";
+import { User } from "./entity/User";
 
 export const resolvers: ResolverMap = {
   Query: {
@@ -6,9 +8,16 @@ export const resolvers: ResolverMap = {
       `Hello ${name || "World"}`
   },
   Mutation: {
-    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {
-      console.log(email);
-      console.log(password);
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = User.create({
+        email: email,
+        password: hashedPassword
+      });
+      await user.save();
       return true;
     }
   }
