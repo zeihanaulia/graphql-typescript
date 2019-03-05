398d0468638c78f6593ef7ab8bfaf616c62940db
Zeihan Aulia <zeihan@warungpintar.co>
2019-03-05 15:07:49 +0700

    implement shcema stitching for separating schema

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,14 +1,25 @@
 import { importSchema } from "graphql-import";
 import { GraphQLServer } from "graphql-yoga";
-import { resolvers } from "./resolvers";
 import * as path from "path";
+import * as fs from "fs";
+import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
 import { createTypeormConnection } from "./utils/createTypeormConnection";
+import { GraphQLSchema } from "graphql";
 // ... or using `require()`
 // const { GraphQLServer } = require('graphql-yoga')
 
 export const startServer = async () => {
-  const typeDefs = importSchema(path.join(__dirname, "schema.graphql"));
-  const server = new GraphQLServer({ typeDefs, resolvers });
+  const schemas: GraphQLSchema[] = [];
+  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
+  folders.forEach(folder => {
+    const { resolvers } = require(`./modules/${folder}/resolvers`);
+    const typeDefs = importSchema(
+      path.join(__dirname, `./modules/${folder}/schema.graphql`)
+    );
+    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
+  });
+
+  const server = new GraphQLServer({ schema: mergeSchemas({ schemas }) });
   await createTypeormConnection();
   const app = await server.start({
     port: process.env.NODE_ENV === "test" ? 0 : 4000
