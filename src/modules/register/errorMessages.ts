c699e68cfc8ff066b917bf07147f91dbfab3901f
Zeihan Aulia <zeihan@warungpintar.co>
2019-03-05 18:56:27 +0700

    add error handling for input

---
+++ src/modules/register/errorMessages.ts
@@ -0,0 +1,4 @@
+export const emailDuplicate = "email already taken";
+export const emailNotLongEnough = "email must be at least 3 character";
+export const emailNotValid = "email must be a valid email";
+export const passwordNotLongEnough = "password must be a valid password";
