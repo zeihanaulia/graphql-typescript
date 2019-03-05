b9b15eca5fc927f4a22d57a9d0dba73aa95d1cde
Zeihan Aulia <zeihan@warungpintar.co>
2019-03-05 17:34:53 +0700

    add error shaping and reuse for other schema

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -13,13 +13,23 @@ export const resolvers: ResolverMap = {
       _,
       { email, password }: GQL.IRegisterOnMutationArguments
     ) => {
+      const userAllreadyExists = await User.findOne({
+        where: { email },
+        select: ["id"]
+      });
+      if (userAllreadyExists) {
+        return [{
+          path: "email",
+          message: "email already taken"
+        }]
+      }
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = User.create({
         email: email,
         password: hashedPassword
       });
       await user.save();
-      return true;
+      return null;
     }
   }
 };
